948410099169b667073421063d9cb4d085497056
Pablo Bonilla <pbonilla29@hotmail.com>
2018-01-08 01:09:29 -0600

    Organized folders
    Added "/src/resources" -> "/css" and "/images"

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -13,7 +14,10 @@ public class Main extends Application {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main_Window.fxml"));
 			Scene scene = new Scene(root, 750, 525);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add("/resources/css/application.css");
+			primaryStage.setResizable(false);
+			primaryStage.getIcons().add(new Image("/resources/images/icon.png"));
+			primaryStage.setTitle("Color Convert");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
